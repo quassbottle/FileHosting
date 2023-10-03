6de8bc974effd79f9bb87054e31af669f3714aa2
quassbottle <quassbottleyt@gmail.com>
2023-10-03 14:42:54 +0300

    fix: migration file name

---
+++ FileHosting.Application/Scripts/001_BaseCreateTables.sql
@@ -0,0 +1,18 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS file_meta (
+    id uuid NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
+    size bigint,
+    name text
+);
+
+CREATE TABLE IF NOT EXISTS file_data (
+    id uuid NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
+    data bytea,
+    meta_id uuid NOT NULL REFERENCES file_meta(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS file_url (
+    id uuid  NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
+    file_id uuid REFERENCES file_data(id) ON DELETE CASCADE
+);
